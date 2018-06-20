96d97f919fdac633a9de4e9aec845cb792575018
Julian Adams <jadams@espol.edu.ec>
2018-06-20 08:04:55 -0500

    Configuracion de dependencias y actualizacion del README

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('dist'));
+
+app.get('/api/getUsername', (req, res) => {
+  res.send({ username: os.userInfo().username })
+});
+
+app.listen(PORT, () => 
+  console.log('[Server]: Listening on port ', PORT)
+);
--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: ['babel-loader']
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
